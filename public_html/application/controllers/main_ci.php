ebc3ba1fd48a4500da11da093c144b2074017c5b
michajlen <elitearty@gmail.com>
2014-06-17 21:44:08 +0300

    fix path to avatar

--- public_html/application/controllers/main_ci.php
+++ public_html/application/controllers/main_ci.php
@@ -14,19 +14,8 @@ class Main_ci extends CI_Controller {
         $result = $post->getAllPosts(0);
         array_pop($result);
         
-//         $postComments = $post->getAllPostComments(5540, 0);	//	id_post
-//         if($postComments['result'] != 0) {
-// 			$postLastCommentDate = $postComments[ $postComments['result'] - 1 ]['date_pc'];
-//         }
-//         else
-// 			$postLastCommentDate = null;
-			
-//         echo "<pre>";
-// 		print_r($postLastCommentDate);
-// 		die();
         
         foreach($result as $key => $val) {
-			//print_r($result[$key]);
 			$result[$key]['date_post'] = $config->ago( strtotime( $result[$key]['date_post'] ) );
 			
 			if ($result[$key]['comments_post'] > 0) {
@@ -41,14 +30,10 @@ class Main_ci extends CI_Controller {
                 $result[$key]['postLastCommentDate'] = '&nbsp;';
             }
 		}
+		
 		$data = array(
 			'post' => $result
 		);
-//        $sql="select * from post as p, profile as pro where pro.id_profile=p.id_profile_post order by date_post desc limit 10";
-//        $this->config_Class->query($sql);
-//		$res = $this->db->query($sql)->result_array();
-// 		echo "<pre>";
-// 		print_r($result);
 		
 		$this->load->view('header');
 		$this->load->view('container', $data);
