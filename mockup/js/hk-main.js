68179b4eac34031a4e551e666108c04f076a244f
romazyan <romazyan@meta.ua>
2014-06-23 13:21:12 +0300

    [+] small changes in main page

---
+++ mockup/js/hk-main.js
@@ -0,0 +1,11 @@
+/*function setHeight($obj){
+    var height = $('.right-col').height();
+    $obj.height(height);
+}
+
+$(document).ready(function(){
+    setHeight($('.side-bar'));
+});
+$(window).resize(function(){
+    setHeight($('.side-bar'));
+});*/
