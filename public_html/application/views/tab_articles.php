75d8490622961ba01837eac8d27dd12dff1a3c17
romazyan <romazyan@meta.ua>
2014-06-23 22:42:50 +0300

    [-] delete old html

--- public_html/application/views/tab_articles.php
+++ public_html/application/views/tab_articles.php
@@ -116,49 +116,6 @@
                 </ul>
             <? endif; ?>
 
-            <ul class="paginator">
-                <? if ($pageNum > 1):?>
-                    <li><a href="tab_articles?pageNum=<? echo $pageNum - 1; ?>">Back</a></li>
-                <? endif; ?>
-
-                <? if ( $pageNum > 0 &&  $pageNum <= $pagerVisibleSigns + 1): ?>
-                    <? for ($i=1; $i<=$pagerVisibleSigns+1; ++$i ): ?>
-                        <li <?php if ($i == $pageNum): ?> class="selected" <?php endif; ?> ><a href="#"><?= $i; ?></a></li>
-                    <? endfor; ?>
-                    <?php if ( $pageNum==2 ): ?>
-                        <li><a href="#"><?= $pageNum+2; ?></a></li>
-                    <?php endif; ?>
-                    <?php if ( $pageNum==3 ): ?>
-                        <li><a href="#"><?= $pageNum+1; ?></a></li>
-                        <li><a href="#"><?= $pageNum+2; ?></a></li>
-                    <?php endif; ?>
-                    <li class="pag-space"><a href="#">...</a></li>
-                    <li><a href="#"><?= $pageCount; ?></a></li>
-
-                <? elseif ( $pageNum <= $pageCount && $pageNum > $pageCount - $pagerVisibleSigns + 1): ?>
-                    <li><a href="#">1</a></li>
-                    <li class="pag-space"><a href="#">...</a></li>
-                    <li <?= $pageNum-2 ?> ><a href="#"><?=  $pageNum-2; ?></a></li>
-                    <li <?= $pageNum-1 ?> ><a href="#"><?=  $pageNum-1; ?></a></li>
-                    <li <? if ($pageNum == $pageCount): ?> class="selected"<? endif; ?> ><a href="#"><? echo $pageCount; ?></a></li>
-
-                <? else: ?>
-                    <li><a href="#">1</a></li>
-                    <li class="pag-space"><a href="#">...</a></li>
-                    <li <?= $pageNum-2 ?> ><a href="#"><?=  $pageNum-2; ?></a></li>
-                    <li <?= $pageNum-1 ?> ><a href="#"><?=  $pageNum-1; ?></a></li>
-                    <li <?= $pageNum ?> class="selected" ><a href="#"><?=  $pageNum; ?></a></li>
-                    <li <?= $pageNum+1 ?>  ><a href="#"><?=  $pageNum+1; ?></a></li>
-                    <li <?= $pageNum+2 ?>  ><a href="#"><?=  $pageNum+2; ?></a></li>
-                    <li class="pag-space"><a href="#">...</a></li>
-                    <li><a href="#"><?= $pageCount; ?></a></li>
-                <? endif; ?>
-
-                <? if ($pageNum < $pageCount): ?>
-                    <li><a href="tab_articles?pageNum=<? echo $pageNum + 1; ?>">Next</a></li>
-                <? endif; ?>
-            </ul>
-
         </div>
 
         <!-- ***************** READ ARTICLE ***************** -->
