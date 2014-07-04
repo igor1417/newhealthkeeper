13bbeba7f0bc3cb8e2f13705ce62a5d7c26016c2
Roman <powerrrg@gmail.com>
2014-07-04 16:07:07 +0300

    test2

--- public_html/api/messageController.php
+++ public_html/api/messageController.php
@@ -26,7 +26,7 @@ class messageController extends Mobile_api {
     
     public function getConversations() {
         $this->answer = $this->_post->getAllConversations($this->getReqParam('timestamp', true, 0));
-        //$this->getDateAgoLastPost();
+        $this->getDateAgoLastPost();
     }
 
     public function getConversationMessages() {
